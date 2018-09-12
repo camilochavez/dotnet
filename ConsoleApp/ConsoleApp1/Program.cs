40fda8feba534404f5531b61352b8c5945032587
Camilo Chavez <darkcamil@hotmial.com>
2018-09-12 11:53:00 -0500

    Programming Fundamentals - C# II

--- ConsoleApp/ConsoleApp1/Program.cs
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shapes;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,37 +7,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int op1;
-            int op2;
-            string operation;
-            Console.WriteLine("Ingrese el primer valor");//Console.WriteLine() is a function which writes in a console
-            op1 = int.Parse(Console.ReadLine());//Console.ReadLine() is a function which reads a string from user
-            Console.WriteLine("Ingrese el segundo valor");
-            op2 = int.Parse(Console.ReadLine());//int.Parse() is a function which converts strings to int
-            Console.WriteLine("Ingrese la operación");
-            operation = Console.ReadLine();
-
-            int result = 0;
-            switch (operation)
-            {
-                case "+":
-                    result = op1 + op2;
-                    break;
-                case "-":
-                    result = op1 - op2;
-                    break;
-                case "*":
-                    result = op1 * op2;
-                    break;
-                case "/":
-                    result = op1 / op2;
-                    break;
-                default:
-                    Console.WriteLine("No se encontró la operación");
-                    break;
-            }
-            Console.WriteLine(result.ToString());
+            int height = 0;
+            int width = 0;
+            Console.WriteLine("Ingrese la altura y el ancho del cuadrado");
+            height = int.Parse(Console.ReadLine());
+            width = int.Parse(Console.ReadLine());
+            Square square = new Square(height);
+            square.Width = width;
+            int area = square.getArea();
+            Console.WriteLine("el area del cuadrado es: " + area);
             Console.Read();
+            //int op1;
+            //int op2;
+            //string operation;
+            //Console.WriteLine("Ingrese el primer valor");//Console.WriteLine() is a function which writes in a console
+            //op1 = int.Parse(Console.ReadLine());//Console.ReadLine() is a function which reads a string from user
+            //Console.WriteLine("Ingrese el segundo valor");
+            //op2 = int.Parse(Console.ReadLine());//int.Parse() is a function which converts strings to int
+            //Console.WriteLine("Ingrese la operación");
+            //operation = Console.ReadLine();
+
+            //int result = 0;
+            //switch (operation)
+            //{
+            //    case "+":
+            //        result = op1 + op2;
+            //        break;
+            //    case "-":
+            //        result = op1 - op2;
+            //        break;
+            //    case "*":
+            //        result = op1 * op2;
+            //        break;
+            //    case "/":
+            //        result = op1 / op2;
+            //        break;
+            //    default:
+            //        Console.WriteLine("No se encontró la operación");
+            //        break;
+            //}
+            //Console.WriteLine(result.ToString());
+            //Console.Read();
         }
     }
 }
---
+++ ConsoleApp/ConsoleApp1/Square.cs
@@ -0,0 +1,20 @@
+﻿namespace Shapes
+{
+    public class Square
+    {
+        private int height;
+        private int width;
+
+        public Square(int height)
+        {
+            this.height = height;            
+        }
+
+        public int Width { get => width; set => width = value; }
+
+        public int getArea()
+        {
+            return height * Width;
+        }
+    }
+}
