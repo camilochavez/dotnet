853588749e7db7e5cbddff7bddc7c457d2cd193e
Camilo Chavez <darkcamil@hotmial.com>
2018-09-14 14:04:49 -0500

    Session Three Strucs, delegates and events

--- ConsoleApp/ConsoleApp1/Program.cs
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -13,41 +13,24 @@ namespace ConsoleApp1
             height = int.Parse(Console.ReadLine());
             width = int.Parse(Console.ReadLine());
             Square square = new Square(height);
+            SquareStruct squareStruct = new SquareStruct(height, width);
             square.Width = width;
+            SetWidthToSquare(square);
             int area = square.getArea();
-            Console.WriteLine("el area del cuadrado es: " + area);
+            Console.WriteLine("el area del cuadrado es: (Class)" + area);
+            SetWidthToSquare(squareStruct);
+            area = squareStruct.getArea();
+            Console.WriteLine("el area del cuadrado es: (Struct)" + area);
             Console.Read();
-            //int op1;
-            //int op2;
-            //string operation;
-            //Console.WriteLine("Ingrese el primer valor");//Console.WriteLine() is a function which writes in a console
-            //op1 = int.Parse(Console.ReadLine());//Console.ReadLine() is a function which reads a string from user
-            //Console.WriteLine("Ingrese el segundo valor");
-            //op2 = int.Parse(Console.ReadLine());//int.Parse() is a function which converts strings to int
-            //Console.WriteLine("Ingrese la operación");
-            //operation = Console.ReadLine();
+        }
 
-            //int result = 0;
-            //switch (operation)
-            //{
-            //    case "+":
-            //        result = op1 + op2;
-            //        break;
-            //    case "-":
-            //        result = op1 - op2;
-            //        break;
-            //    case "*":
-            //        result = op1 * op2;
-            //        break;
-            //    case "/":
-            //        result = op1 / op2;
-            //        break;
-            //    default:
-            //        Console.WriteLine("No se encontró la operación");
-            //        break;
-            //}
-            //Console.WriteLine(result.ToString());
-            //Console.Read();
+        static void SetWidthToSquare(SquareStruct square)
+        {
+            square.Width = 50;
+        }
+        static void SetWidthToSquare(Square square)
+        {
+            square.Width = 50;
         }
     }
 }
--- ConsoleApp/ConsoleApp1/Square.cs
+++ ConsoleApp/ConsoleApp1/Square.cs
@@ -17,4 +17,23 @@
             return height * Width;
         }
     }
+
+    public struct SquareStruct
+    {
+        private int height;
+        private int width;
+
+        public SquareStruct(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public int Width { get => width; set => width = value; }
+
+        public int getArea()
+        {
+            return height * Width;
+        }
+    }
 }
