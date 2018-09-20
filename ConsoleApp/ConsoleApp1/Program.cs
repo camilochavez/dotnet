c5f308f0ea3671da9abbcfe3138b8ff253d2a349
Camilo Chavez <darkcamil@hotmial.com>
2018-09-20 10:43:26 -0500

    Events in dot .net

--- ConsoleApp/ConsoleApp1/Program.cs
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -13,41 +13,16 @@ namespace ConsoleApp1
             height = int.Parse(Console.ReadLine());
             width = int.Parse(Console.ReadLine());
             Square square = new Square(height);
+            square.ChangeWidth += new EventHandler(Square_Change);
             square.Width = width;
-            int area = square.getArea();
+            int area = square.GetArea();
             Console.WriteLine("el area del cuadrado es: " + area);
             Console.Read();
-            //int op1;
-            //int op2;
-            //string operation;
-            //Console.WriteLine("Ingrese el primer valor");//Console.WriteLine() is a function which writes in a console
-            //op1 = int.Parse(Console.ReadLine());//Console.ReadLine() is a function which reads a string from user
-            //Console.WriteLine("Ingrese el segundo valor");
-            //op2 = int.Parse(Console.ReadLine());//int.Parse() is a function which converts strings to int
-            //Console.WriteLine("Ingrese la operación");
-            //operation = Console.ReadLine();
+        }
 
-            //int result = 0;
-            //switch (operation)
-            //{
-            //    case "+":
-            //        result = op1 + op2;
-            //        break;
-            //    case "-":
-            //        result = op1 - op2;
-            //        break;
-            //    case "*":
-            //        result = op1 * op2;
-            //        break;
-            //    case "/":
-            //        result = op1 / op2;
-            //        break;
-            //    default:
-            //        Console.WriteLine("No se encontró la operación");
-            //        break;
-            //}
-            //Console.WriteLine(result.ToString());
-            //Console.Read();
+        static void Square_Change(object sender, EventArgs eventArgs)
+        {
+            Console.WriteLine("El ancho del cuadrado cambió: nuevo valor " + ((WidthEventArgs)eventArgs).NewWidth + " Valor anterior " + ((WidthEventArgs)eventArgs).OldWidth);
         }
     }
 }
--- ConsoleApp/ConsoleApp1/Square.cs
+++ ConsoleApp/ConsoleApp1/Square.cs
@@ -1,20 +1,42 @@
-﻿namespace Shapes
+﻿using System;
+
+namespace Shapes
 {
     public class Square
     {
         private int height;
         private int width;
+        public event EventHandler ChangeWidth;
 
         public Square(int height)
         {
-            this.height = height;            
+            this.height = height;
         }
 
-        public int Width { get => width; set => width = value; }
+        public int Width
+        {
+            get => width;
+            set
+            {
+                ChangeWidth(this, new WidthEventArgs(value, width));
+                width = value;
+            }
+        }
 
-        public int getArea()
+        public int GetArea()
         {
             return height * Width;
+        }
+    }
+
+    public class WidthEventArgs : EventArgs
+    {
+        public int NewWidth { get; set; }
+        public int OldWidth { get; set; }
+        public WidthEventArgs(int newWidth, int oldWidth)
+        {
+            NewWidth = newWidth;
+            OldWidth = oldWidth;
         }
     }
 }
